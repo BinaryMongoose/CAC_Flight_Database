1075a993702e8d8d7951a23d74380c983684942b
BinaryMongoose <96893291+BinaryMongoose@users.noreply.github.com>
2023-04-08 14:35:31 -0700

    Revert "Finished basic file slicing."
    This reverts commit bd79c81fe8f4ebe22b51315cdac7ffd94300ef17.

--- src/Database.py
+++ src/Database.py
@@ -3,36 +3,30 @@
 """
 import csv, sys, os
 
-files = {}
+data_folder = "../data/RAW_HB/"
+
 restart_count = 0
+
+def count_files():
+    flight_files = os.listdir(data_folder)
+    return len(flight_files)
     
-header_found = False;
 
 filename = '../data/RAW_HB/FILE_0.csv'
 with open(filename, newline='') as f:
     reader = csv.reader(f)
     try:
-        i = 0
         for row in reader:
+            # print(row)
             if 'time' in row[0]:
-                count = 0
-                
-                while True:
-                    if 'time' in row[0]:
-                        break
-                    count += 1
-                files[i] = count
-            i += 1
-            
-
-        # print(files)
+                restart_count += 1
 
     except csv.Error as e:
         sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
 
-for index, count in files.items():
-    print(f"There are {count} lines in header section {index}")
+    print(f"There are {count_files()} files in the folder.")
+    print(f"The payload restarted {restart_count} times.")
 
-print(f'FILE_0 has {len(files.values())} headers.')
-
+def parse_file(header, pattern):
+    pass    
 
--- src/Panda_Database.py
+++ src/Panda_Database.py
@@ -4,6 +4,7 @@ import pandas as pd
 data = pd.read_csv("../data/RAW_HB/FILE_0.csv", low_memory=False)
 
 # Output the number of rows
-print(f"Total rows: {len(data)}")
+print("Total rows: {0}".format(len(data)))
 
-
+# See which headers are available
+print(list(data))
